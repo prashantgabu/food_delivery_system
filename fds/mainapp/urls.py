d0e064eb1528364a15244f6d065c0f40153a1d65
prashantgabu <prashantg5399@gmail.com>
2020-09-04 21:11:25 +0530

    addDish error

--- fds/mainapp/urls.py
+++ fds/mainapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.res_login, name='res_login'),
     path('res_register', views.res_register, name='res_register'),
+    path('res_dashboard', views.res_dashboard, name='res_dashboard'),
+    path('res_addDish', views.res_addDish, name='res_addDish'),
+    path('res_viewDish', views.res_viewDish, name='res_viewDish'),
 
 ]
--- fds/mainapp/views.py
+++ fds/mainapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, redirect
-from . models import Restaurant
+from . models import Restaurant, Dish
+from django.contrib import messages
 
 
 def res_login(request):
-    return render(request, 'backend/res_login.html')
+    try:
+        if request.method == "POST":
+            email1 = request.POST.get('email')
+            password = request.POST.get('password')
+            restaurant = Restaurant.objects.get(email=email1)
+            check_auth = restaurant.email == email1 and restaurant.password == password
+            if (check_auth):
+                return redirect('res_dashboard')
+            else:
+                messages.error(request, 'Password incorrect.')
+                return redirect('res_login')
+        else:
+            return render(request, 'backend/res_login.html')
+    except Restaurant.DoesNotExist:
+        restaurant = None
+        return render(request, 'backend/res_login.html')
 
 
 def res_register(request):
@@ -21,3 +37,28 @@ def res_register(request):
         return redirect('res_login')
     else:
         return render(request, 'backend/res_register.html')
+
+
+def res_dashboard(request):
+    return render(request, 'backend/res_dashboard.html')
+
+
+def res_addDish(request):
+    if request.method == "POST":
+        dishname = request.POST.get('dishname')
+        dishprice = request.POST.get('dishprice')
+        dishdescription = request.POST.get('dishdescription')
+        customization = request.POST.get('customization')
+        dishimage = request.POST.get('dishimage')
+        cuisine = request.POST.get('cuisine')
+        dish = Dish(
+            dish_name=dishname, price=dishprice, dish_description=dishdescription, customization=customization, dish_photo=dishimage)
+        dish.save()
+        print("Dish object:", dish)
+        return redirect('res_viewDish')
+    else:
+        return render(request, 'backend/res_addDish.html')
+
+
+def res_viewDish(request):
+    return render(request, 'backend/viewDish.html')
