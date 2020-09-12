f4b1b23d479bd9058f455a2f52af678099809604
prashantgabu <prashantg5399@gmail.com>
2020-09-12 22:32:15 +0530

    cart remove

--- fds/buyer/urls.py
+++ fds/buyer/urls.py
@@ -4,5 +4,13 @@ urlpatterns = [
     path('', views.buyer_dashboard, name='buyer_dashboard'),
     path('buyer_register', views.buyer_register, name='buyer_register'),
     path('buyer_login', views.buyer_login, name='buyer_login'),
+    path('buyer_logout', views.buyer_logout, name='buyer_logout'),
+    path('buyer_viewDishByCuisine/<int:id>', views.buyer_viewDishByCuisine, name='buyer_viewDishByCuisine'),
+    path('buyer_viewDishByRestaurant/<int:id>', views.buyer_viewDishByRestaurant, name='buyer_viewDishByRestaurant'),
+    path('buyer_viewDish', views.buyer_viewDish, name='buyer_viewDish'),
+    path('buyer_viewDiscount', views.buyer_viewDiscount, name='buyer_viewDiscount'),
+    path('buyer_addToCart', views.buyer_addToCart, name='buyer_addToCart'),
+    path('buyer_viewCart', views.buyer_viewCart, name='buyer_viewCart'),
+    path('buyer_removeDish/<int:id>', views.buyer_removeDish, name='buyer_removeDish'),
 
 ]
--- fds/buyer/views.py
+++ fds/buyer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from mainapp.models import Reg_user, Restaurant, Delivery_agent, Order, Cuisine, Report, Dish, Ambience, Verification
+from mainapp.models import Reg_user, Restaurant, Delivery_agent, Order, Cuisine, Report, Dish, Ambience, Verification, Discount, Cart
 from django.core.mail import send_mail
 
 
@@ -23,6 +23,7 @@ def buyer_login(request):
         reg_user = None
         return render(request, 'buyer/buyer_dashboard.html')
 
+
 def buyer_register(request):
     if request.method == "POST":
         fname = request.POST.get('fname')
@@ -43,4 +44,103 @@ def buyer_register(request):
 
 
 def buyer_dashboard(request):
-    return render(request, "buyer/buyer_dashboard.html")
+    if "buyer_id" in request.session:
+        buyer_id = request.session['buyer_id']
+    else:
+        buyer_id = False
+    restaurant_list = Restaurant.objects.all()
+    cuisine_list = Cuisine.objects.all()
+    return render(request, "buyer/buyer_dashboard.html", {'buyer_id': buyer_id, "cuisine_list": cuisine_list, 'restaurant_list': restaurant_list})
+
+
+def buyer_viewDishByCuisine(request, id):
+    if "buyer_id" in request.session:
+        buyer_id = request.session['buyer_id']
+    else:
+        buyer_id = False
+    restaurant_list = Restaurant.objects.all()
+    cuisine_list = Cuisine.objects.all()
+    cuisine_by_id = Cuisine.objects.get(id=id)
+    dish_list = Dish.objects.filter(cuisine_id=id)
+    cuisine_name = ""
+    cuisine_name = cuisine_by_id.cuisine_name
+    return render(request, "buyer/buyer_viewDishByCuisine.html", {'buyer_id': buyer_id, "dish_list": dish_list, 'cuisine_list': cuisine_list, "cuisine_name": cuisine_name, 'restaurant_list': restaurant_list})
+
+
+def buyer_viewDishByRestaurant(request, id):
+    if "buyer_id" in request.session:
+        buyer_id = request.session['buyer_id']
+    else:
+        buyer_id = False
+    dishlist_by_restautrant_id = Dish.objects.filter(restaurant_id=id)
+    restaurant_by_id = Restaurant.objects.get(id=id)
+    restaurant_name = restaurant_by_id.name
+    cuisine_list = Cuisine.objects.all()
+    restaurant_list = Restaurant.objects.all()
+    return render(request, "buyer/buyer_viewDishByRestaurant.html", {'buyer_id': buyer_id, "dishlist_by_restautrant_id": dishlist_by_restautrant_id, 'cuisine_list': cuisine_list, "restaurant_name": restaurant_name, 'restaurant_list': restaurant_list})
+
+
+def buyer_viewDish(request):
+    if "buyer_id" in request.session:
+        buyer_id = request.session['buyer_id']
+    else:
+        buyer_id = False
+    dish_list = Dish.objects.all()
+    cuisine_list = Cuisine.objects.all()
+    restaurant_list = Restaurant.objects.all()
+    return render(request, "buyer/buyer_viewDish.html", {'buyer_id': buyer_id, "dish_list": dish_list, 'cuisine_list': cuisine_list, 'restaurant_list': restaurant_list})
+
+
+def buyer_viewDiscount(request):
+    if "buyer_id" in request.session:
+        buyer_id = request.session['buyer_id']
+    else:
+        buyer_id = False
+    discount_list = Discount.objects.all()
+    cuisine_list = Cuisine.objects.all()
+    restaurant_list = Restaurant.objects.all()
+    return render(request, "buyer/buyer_viewDiscount.html", {'buyer_id': buyer_id, "discount_list": discount_list, 'cuisine_list': cuisine_list, 'restaurant_list': restaurant_list})
+
+
+def buyer_addToCart(request):
+    if request.method == "POST":
+        if "buyer_id" in request.session:
+            buyer_id = request.session['buyer_id']
+        else:
+            buyer_id = False
+        dish_id = request.POST.get('dish_id')
+        quantity = request.POST.get('quantity')
+        dish_by_id = Dish.objects.get(id=dish_id)
+        price = dish_by_id.price
+        print("PRICE AND QTY", price, quantity)
+        total_amount = (float(price)*float(quantity))
+        print("Total Amount",total_amount)
+        restaurant_id = dish_by_id.restaurant_id.id
+        discount = Discount.objects.get(restaurant_id=restaurant_id)
+        discount_id = discount.id
+        buyer=Reg_user.objects.get(id=buyer_id)
+        cart = Cart(total_amount=total_amount, status="added", quantity=quantity,
+                    discount_id=discount, dish_id=dish_by_id, reg_user_id=buyer)
+        cart.save()
+        return redirect('buyer_viewCart')
+
+def buyer_viewCart(request):
+    if "buyer_id" in request.session:
+        buyer_id = request.session['buyer_id']
+    else:
+        buyer_id = False
+    discount_list = Discount.objects.all()
+    cuisine_list = Cuisine.objects.all()
+    restaurant_list = Restaurant.objects.all()
+    cart_list=Cart.objects.filter(reg_user_id=buyer_id)
+    return render(request, "buyer/buyer_viewCart.html", {'buyer_id': buyer_id, "discount_list": discount_list, 'cuisine_list': cuisine_list, 'restaurant_list': restaurant_list,'cart_list': cart_list})
+
+def buyer_removeDish(request, id):
+    cart = Cart.objects.get(id=id)
+    cart.delete()
+    return redirect('buyer_viewCart')
+
+
+def buyer_logout(request):
+    request.session.delete()
+    return redirect('buyer_dashboard')
